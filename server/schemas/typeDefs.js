937a525de86c07aa8aa2908685842a2a5246e480
honguyen00 <lehonguyen00@gmail.com>
2024-01-22 22:26:41 +1100

    initial commit

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const typeDefs = `
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Integer
+        savedBooks: [Book]!
+    }
+
+    type Book {
+        bookId: ID,
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
+
+    input NewBookInput {
+        bookId: ID!, 
+        authors: [String]!, 
+        description: String!, 
+        title: String!, 
+        image: String!, 
+        link: String!
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: NewBookInput): User
+        removeBook(bookId: ID!): User
+    }
+`
+
+module.exports = typeDefs;
